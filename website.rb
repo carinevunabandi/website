5a542ba924cc8a61072414c8949b70a46df0b637
Carine Vunabandi <carinevun@yahoo.com>
2015-02-11 13:35:33 +0000

    BasicCSS: Apply some general CSS style to the layout

--- website.rb
+++ website.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 
 class Website < Sinatra::Base
   set :static, true
-  set :public_dir, File.dirname(__FILE__) + '/static'
+  #set :public_dir, File.dirname(__FILE__) + '/static'
 
   Dir[File.join(Sinatra::Application.root, 'app', 'controllers', '*.rb')].each { |f| require f }
   Dir[File.join(Sinatra::Application.root, 'app', 'models', '*.rb')].each { |f| require f }
